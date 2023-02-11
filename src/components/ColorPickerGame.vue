0e2d18f1b0cc4fcfb567b6194283a810ef57a7d4
Egu Chinedu <eguchinedu18@gmail.com>
2023-02-11 15:59:48 +0100

    colorpicker converted to composables

--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -1,13 +1,24 @@
 <template>
   <h1>Color Picker Game</h1>
-  <colorPicker/>
+
+  <div>{{ message }}</div>
+
+  <button v-for="color in colors" :key="color" @click="matchColor(color)">
+    {{ color }}
+  </button>
 </template>
 
 <script>
-import colorPicker from '../composables/colorPicker.vue';
+import ColorPicker from "../composables/colorPicker.js";
 export default {
- components: {colorPicker}
+  setup() {
+    const { colors, message, matchColor } = ColorPicker();
+    return { colors, message, matchColor };
+  },
 };
 </script>
 
-<style></style>
+
+<style>
+
+</style>
